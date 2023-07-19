48ae573278e1c945e8bea45af61b0f1f2ca61b95
titan <titan@mail.ru>
2023-07-19 23:43:36 +0500

    создан home.blade.php и настроен маршрут на /

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+    // return view('home');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
